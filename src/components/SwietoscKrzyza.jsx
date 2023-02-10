e014c97d0ab9f37ad8a6e6a556f6ad9f579e9005
Joszko <jakub.rezler96@gmail.com>
2023-02-10 22:47:31 +0100

    create audio play mechanics in new piece component

--- src/components/SwietoscKrzyza.jsx
+++ src/components/SwietoscKrzyza.jsx
@@ -1,7 +1,115 @@
 import React from "react";
 
 const SwietoscKrzyza = () => {
-	return <div></div>;
+	const [voice, setVoice] = useState({
+		sopran: false,
+		alt: false,
+		tenor: false,
+		bas: false,
+	});
+
+	const { sopran, alt, tenor, bas } = voice;
+
+	const handleInputChange = (e) => {
+		setVoice({
+			...voice,
+			[e.target.id]: !voice[e.target.id],
+		});
+	};
+	return (
+		<div className="down-content">
+			<h2 className="piece-name-header">{props.state.pieceName}</h2>
+			<div className="select-voice-container">
+				<label htmlFor="sopran">
+					Sopran
+					<input
+						type="checkbox"
+						id="sopran"
+						onChange={handleInputChange}
+						checked={sopran}
+					/>
+				</label>
+				<label htmlFor="alt">
+					Alt
+					<input
+						type="checkbox"
+						id="alt"
+						onChange={handleInputChange}
+						checked={alt}
+					/>
+				</label>
+				<label htmlFor="tenor">
+					Tenor
+					<input
+						type="checkbox"
+						id="tenor"
+						onChange={handleInputChange}
+						checked={tenor}
+					/>
+				</label>
+				<label htmlFor="bas">
+					Bas
+					<input
+						type="checkbox"
+						id="bas"
+						onChange={handleInputChange}
+						checked={bas}
+					/>
+				</label>
+			</div>
+
+			<div className="audio-container">
+				{sopran && !alt && !tenor && !bas ? (
+					<audio src={ksSopran} id="sopran-audio" controls />
+				) : null}
+				{alt && !sopran && !tenor && !bas ? (
+					<audio src={ksAlt} id="alt-audio" controls />
+				) : null}
+				{tenor && !sopran && !alt && !bas ? (
+					<audio src={ksTenor} id="tenor-audio" controls />
+				) : null}
+				{bas && !sopran && !alt && !tenor ? (
+					<audio src={ksBas} id="bas-audio" controls />
+				) : null}
+				{sopran && alt && !tenor && !bas ? (
+					<audio src={ksSopranAlt} id="sopran-alt-audio" controls />
+				) : null}
+				{sopran && tenor && !alt && !bas ? (
+					<audio src={ksSopranTenor} id="sopran-tenor-audio" controls />
+				) : null}
+				{sopran && bas && !alt && !tenor ? (
+					<audio src={ksSopranBass} id="sopran-bas-audio" controls />
+				) : null}
+				{alt && tenor && !sopran && !bas ? (
+					<audio src={ksAltTenor} id="alt-tenor-audio" controls />
+				) : null}
+				{alt && bas && !sopran && !tenor ? (
+					<audio src={ksAltBass} id="alt-bas-audio" controls />
+				) : null}
+				{tenor && bas && !sopran && !alt ? (
+					<audio src={ksTenorBass} id="tenor-bas-audio" controls />
+				) : null}
+				{sopran && alt && tenor && !bas ? (
+					<audio src={ksSopranAltTenor} id="sopran-alt-tenor-audio" controls />
+				) : null}
+				{sopran && tenor && bas && !alt ? (
+					<audio src={ksSopranTenorBass} id="sopran-tenor-bas-audio" controls />
+				) : null}
+				{sopran && alt && bas && !tenor ? (
+					<audio src={ksSopranAltBass} id="sopran-alt-bas-audio" controls />
+				) : null}
+				{alt && tenor && bas && !sopran ? (
+					<audio src={ksAltTenorBass} id="alt-tenor-bas-audio" controls />
+				) : null}
+				{sopran && alt && tenor && bas ? (
+					<audio src={ksAll} id="all-audio" controls />
+				) : null}
+			</div>
+			<div className="notes-container">
+				<img src={img2} className="notes" alt="Krzyżu Święty - nuty" />
+			</div>
+		</div>
+	);
 };
 
 export default SwietoscKrzyza;
