9c458ceeae23d7037ead687b87bb2fd2f2f1fec4
Joszko <jakub.rezler96@gmail.com>
2023-02-10 22:50:28 +0100

    adjust variables name and path to files in new piece component

--- src/components/SwietoscKrzyza.jsx
+++ src/components/SwietoscKrzyza.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import "../style/css/DownContent.css";
-import ksSopran from "../audio/krzyzu-swiety/sopran.mp3";
-import ksAlt from "../audio/krzyzu-swiety/alt.mp3";
-import ksTenor from "../audio/krzyzu-swiety/tenor.mp3";
-import ksBas from "../audio/krzyzu-swiety/bas.mp3";
-import ksSopranAlt from "../audio/krzyzu-swiety/sopran-alt.mp3";
-import ksSopranTenor from "../audio/krzyzu-swiety/sopran-tenor.mp3";
-import ksSopranBass from "../audio/krzyzu-swiety/sopran-bas.mp3";
-import ksAltTenor from "../audio/krzyzu-swiety/alt-tenor.mp3";
-import ksAltBass from "../audio/krzyzu-swiety/alt-bas.mp3";
-import ksTenorBass from "../audio/krzyzu-swiety/tenor-bas.mp3";
-import ksSopranAltTenor from "../audio/krzyzu-swiety/sopran-alt-tenor.mp3";
-import ksSopranTenorBass from "../audio/krzyzu-swiety/sopran-tenor-bas.mp3";
-import ksSopranAltBass from "../audio/krzyzu-swiety/sopran-alt-bas.mp3";
-import ksAltTenorBass from "../audio/krzyzu-swiety/alt-tenor-bas.mp3";
-import ksAll from "../audio/krzyzu-swiety/all.mp3";
-import img2 from "../img/krzyzu_swiety_nade_wszystko-1.jpg";
+import skSopran from "../audio/swietosc-krzyza/sopran.mp3";
+import skAlt from "../audio/swietosc-krzyza/alt.mp3";
+import skTenor from "../audio/swietosc-krzyza/tenor.mp3";
+import skBas from "../audio/swietosc-krzyza/bas.mp3";
+import skSopranAlt from "../audio/swietosc-krzyza/sopran-alt.mp3";
+import skSopranTenor from "../audio/swietosc-krzyza/sopran-tenor.mp3";
+import skSopranBass from "../audio/swietosc-krzyza/sopran-bas.mp3";
+import skAltTenor from "../audio/swietosc-krzyza/alt-tenor.mp3";
+import skAltBass from "../audio/swietosc-krzyza/alt-bas.mp3";
+import skTenorBass from "../audio/swietosc-krzyza/tenor-bas.mp3";
+import skSopranAltTenor from "../audio/swietosc-krzyza/sopran-alt-tenor.mp3";
+import skSopranTenorBass from "../audio/swietosc-krzyza/sopran-tenor-bas.mp3";
+import skSopranAltBass from "../audio/swietosc-krzyza/sopran-alt-bas.mp3";
+import skAltTenorBass from "../audio/swietosc-krzyza/alt-tenor-bas.mp3";
+import skAll from "../audio/swietosc-krzyza/all.mp3";
+import img5 from "../img/swietosc-krzyza.jpg";
 
 const SwietoscKrzyza = () => {
 	const [voice, setVoice] = useState({
@@ -77,49 +77,49 @@ const SwietoscKrzyza = () => {
 
 			<div className="audio-container">
 				{sopran && !alt && !tenor && !bas ? (
-					<audio src={ksSopran} id="sopran-audio" controls />
+					<audio src={skSopran} id="sopran-audio" controls />
 				) : null}
 				{alt && !sopran && !tenor && !bas ? (
-					<audio src={ksAlt} id="alt-audio" controls />
+					<audio src={skAlt} id="alt-audio" controls />
 				) : null}
 				{tenor && !sopran && !alt && !bas ? (
-					<audio src={ksTenor} id="tenor-audio" controls />
+					<audio src={skTenor} id="tenor-audio" controls />
 				) : null}
 				{bas && !sopran && !alt && !tenor ? (
-					<audio src={ksBas} id="bas-audio" controls />
+					<audio src={skBas} id="bas-audio" controls />
 				) : null}
 				{sopran && alt && !tenor && !bas ? (
-					<audio src={ksSopranAlt} id="sopran-alt-audio" controls />
+					<audio src={skSopranAlt} id="sopran-alt-audio" controls />
 				) : null}
 				{sopran && tenor && !alt && !bas ? (
-					<audio src={ksSopranTenor} id="sopran-tenor-audio" controls />
+					<audio src={skSopranTenor} id="sopran-tenor-audio" controls />
 				) : null}
 				{sopran && bas && !alt && !tenor ? (
-					<audio src={ksSopranBass} id="sopran-bas-audio" controls />
+					<audio src={skSopranBass} id="sopran-bas-audio" controls />
 				) : null}
 				{alt && tenor && !sopran && !bas ? (
-					<audio src={ksAltTenor} id="alt-tenor-audio" controls />
+					<audio src={skAltTenor} id="alt-tenor-audio" controls />
 				) : null}
 				{alt && bas && !sopran && !tenor ? (
-					<audio src={ksAltBass} id="alt-bas-audio" controls />
+					<audio src={skAltBass} id="alt-bas-audio" controls />
 				) : null}
 				{tenor && bas && !sopran && !alt ? (
-					<audio src={ksTenorBass} id="tenor-bas-audio" controls />
+					<audio src={skTenorBass} id="tenor-bas-audio" controls />
 				) : null}
 				{sopran && alt && tenor && !bas ? (
-					<audio src={ksSopranAltTenor} id="sopran-alt-tenor-audio" controls />
+					<audio src={skSopranAltTenor} id="sopran-alt-tenor-audio" controls />
 				) : null}
 				{sopran && tenor && bas && !alt ? (
-					<audio src={ksSopranTenorBass} id="sopran-tenor-bas-audio" controls />
+					<audio src={skSopranTenorBass} id="sopran-tenor-bas-audio" controls />
 				) : null}
 				{sopran && alt && bas && !tenor ? (
-					<audio src={ksSopranAltBass} id="sopran-alt-bas-audio" controls />
+					<audio src={skSopranAltBass} id="sopran-alt-bas-audio" controls />
 				) : null}
 				{alt && tenor && bas && !sopran ? (
-					<audio src={ksAltTenorBass} id="alt-tenor-bas-audio" controls />
+					<audio src={skAltTenorBass} id="alt-tenor-bas-audio" controls />
 				) : null}
 				{sopran && alt && tenor && bas ? (
-					<audio src={ksAll} id="all-audio" controls />
+					<audio src={skAll} id="all-audio" controls />
 				) : null}
 			</div>
 			<div className="notes-container">
