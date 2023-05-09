b9d4e5e72ebd46673a551a9462425dff0ca60773
Joszko <jakub.rezler96@gmail.com>
2023-05-09 18:19:17 +0200

    update new component

--- src/components/UkazMiPanie.jsx
+++ src/components/UkazMiPanie.jsx
@@ -78,53 +78,57 @@ const UkazMiPanie = (props) => {
 
 			<div className="audio-container">
 				{sopran && !alt && !tenor && !bas ? (
-					<audio src={pPSopran} id="sopran-audio" controls />
+					<audio src={umPSopran} id="sopran-audio" controls />
 				) : null}
 				{alt && !sopran && !tenor && !bas ? (
-					<audio src={pPAlt} id="alt-audio" controls />
+					<audio src={umPAlt} id="alt-audio" controls />
 				) : null}
 				{tenor && !sopran && !alt && !bas ? (
-					<audio src={pPTenor} id="tenor-audio" controls />
+					<audio src={umPTenor} id="tenor-audio" controls />
 				) : null}
 				{bas && !sopran && !alt && !tenor ? (
-					<audio src={pPBas} id="bas-audio" controls />
+					<audio src={umPBas} id="bas-audio" controls />
 				) : null}
 				{sopran && alt && !tenor && !bas ? (
-					<audio src={pPSopranAlt} id="sopran-alt-audio" controls />
+					<audio src={umPSopranAlt} id="sopran-alt-audio" controls />
 				) : null}
 				{sopran && tenor && !alt && !bas ? (
-					<audio src={pPSopranTenor} id="sopran-tenor-audio" controls />
+					<audio src={umPSopranTenor} id="sopran-tenor-audio" controls />
 				) : null}
 				{sopran && bas && !alt && !tenor ? (
-					<audio src={pPSopranBass} id="sopran-bas-audio" controls />
+					<audio src={umPSopranBass} id="sopran-bas-audio" controls />
 				) : null}
 				{alt && tenor && !sopran && !bas ? (
-					<audio src={pPAltTenor} id="alt-tenor-audio" controls />
+					<audio src={umPAltTenor} id="alt-tenor-audio" controls />
 				) : null}
 				{alt && bas && !sopran && !tenor ? (
-					<audio src={pPAltBass} id="alt-bas-audio" controls />
+					<audio src={umPAltBass} id="alt-bas-audio" controls />
 				) : null}
 				{tenor && bas && !sopran && !alt ? (
-					<audio src={pPTenorBass} id="tenor-bas-audio" controls />
+					<audio src={umPTenorBass} id="tenor-bas-audio" controls />
 				) : null}
 				{sopran && alt && tenor && !bas ? (
-					<audio src={pPSopranAltTenor} id="sopran-alt-tenor-audio" controls />
+					<audio src={umPSopranAltTenor} id="sopran-alt-tenor-audio" controls />
 				) : null}
 				{sopran && tenor && bas && !alt ? (
-					<audio src={pPSopranTenorBass} id="sopran-tenor-bas-audio" controls />
+					<audio
+						src={umPSopranTenorBass}
+						id="sopran-tenor-bas-audio"
+						controls
+					/>
 				) : null}
 				{sopran && alt && bas && !tenor ? (
-					<audio src={pPSopranAltBass} id="sopran-alt-bas-audio" controls />
+					<audio src={umPSopranAltBass} id="sopran-alt-bas-audio" controls />
 				) : null}
 				{alt && tenor && bas && !sopran ? (
-					<audio src={pPAltTenorBass} id="alt-tenor-bas-audio" controls />
+					<audio src={umPAltTenorBass} id="alt-tenor-bas-audio" controls />
 				) : null}
 				{sopran && alt && tenor && bas ? (
-					<audio src={pPAll} id="all-audio" controls />
+					<audio src={umPAll} id="all-audio" controls />
 				) : null}
 			</div>
 			<div className="notes-container">
-				<img src={img} className="notes" alt="Przychodzisz Panie - nuty" />
+				<img src={img} className="notes" alt="Ukaż mi Panie - nuty" />
 			</div>
 		</div>
 	);
